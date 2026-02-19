bd3745d92e66111a5fa9b596a668f9f157bc1447
Andrew Mercurio <drewmerc@gmail.com>
2026-02-19 15:42:20 -0500

    feat(components): add barrel export

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+// src/components/index.ts
+export { GuitarNeck } from './GuitarNeck';
+export { NotePicker } from './NotePicker';
+export { Picker } from './Picker';
+export { DisplayToggle } from './DisplayToggle';
+export { ChordPreview } from './ChordPreview';
+export { StringGroupPicker } from './StringGroupPicker';
+export { ScalePositionPicker } from './ScalePositionPicker';
