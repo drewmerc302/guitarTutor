5fc82e5f5ee97b3976cf1032d156dac728c93db6
Andrew Mercurio <drewmerc@gmail.com>
2026-02-20 13:14:05 -0500

    feat(components): export ChordDiagram

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,11 @@
 // src/components/index.ts
+export { ErrorBoundary } from './ErrorBoundary';
 export { GuitarNeck } from './GuitarNeck';
+export { FretboardViewer } from './FretboardViewer';
 export { NotePicker } from './NotePicker';
-export { Picker } from './Picker';
+export { TypePicker } from './TypePicker';
 export { DisplayToggle } from './DisplayToggle';
 export { ChordPreview } from './ChordPreview';
+export { ChordDiagram } from './ChordDiagram';
 export { StringGroupPicker } from './StringGroupPicker';
 export { ScalePositionPicker } from './ScalePositionPicker';
