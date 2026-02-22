3e6ef9037442915477ddfa200f06ca885a7c32e6
Andrew Mercurio <drewmerc@gmail.com>
2026-02-22 14:29:29 -0500

    chore: remove duplicate expoVectorIcons mock

--- src/__mocks__/expoVectorIcons.js
+++
@@ -1,9 +0,0 @@
-const React = require('react');
-
-const MockIcon = ({ name, size, color, ...props }) =>
-  React.createElement('MaterialCommunityIcons', { name, size, color, testID: name, ...props });
-
-MockIcon.displayName = 'MaterialCommunityIcons';
-
-module.exports = MockIcon;
-module.exports.default = MockIcon;
